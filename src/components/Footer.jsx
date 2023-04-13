5cc9dc3befee368de28be9e699b16197c4bbbf2c
CoderAyushjain <110177124+CoderAyushjain@users.noreply.github.com>
2023-04-13 17:02:56 +0530

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/@ayushjain924" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/Ashu_9770" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/CoderAyushjain" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
